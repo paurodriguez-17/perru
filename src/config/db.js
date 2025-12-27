84585048a162725c39c4a8ffaef5aae18f759160
paurodriguez-17 <paurodriguez0617@gmail.com>
2025-12-27 14:37:34 -0300

    hora agenda

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
 const { createPool } = require('mysql2/promise');
-require('dotenv').config(); 
+require('dotenv').config();
 
 const pool = createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Coco1406.', 
+    password: process.env.DB_PASSWORD || 'Coco1406.',
     database: process.env.DB_NAME || 'perruqueria',
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    dateStrings: true 
 });
 
 module.exports = pool;
