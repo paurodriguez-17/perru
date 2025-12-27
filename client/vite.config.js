61c541128f729e096bee90b59736391cc01f279f
paurodriguez-17 <paurodriguez0617@gmail.com>
2025-12-27 13:03:48 -0300

    PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Perruquería Spa & Shop',
+        short_name: 'Perruquería',
+        description: 'Sistema de Gestión Integral para Mascotas',
+        theme_color: '#ffffff',
+        background_color: '#FFFDFE',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
